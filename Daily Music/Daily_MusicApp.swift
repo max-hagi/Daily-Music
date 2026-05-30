3da48cec1e4db5848d49e7a572cd37c433ecad34
Maxime Savehilaghi <msavehilaghi99@gmail.com>
2026-05-30 15:16:56 -0400

    Initial Commit

---
+++ Daily Music/Daily_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_MusicApp.swift
+//  Daily Music
+//
+//  Created by Maxime Savehilaghi on 2026-05-30.
+//
+
+import SwiftUI
+
+@main
+struct Daily_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
